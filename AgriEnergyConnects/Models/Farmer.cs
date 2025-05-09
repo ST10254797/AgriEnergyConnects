93d1da0fe93fc504f79538a2d11cadf8ce766d6b
ST10254797 <cristianoronaldonaidoo@gmail.com>
2025-05-09 18:24:53 +0200

    Added the farmers class

---
+++ AgriEnergyConnects/Models/Farmer.cs
@@ -0,0 +1,25 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace AgriEnergyConnects.Models
+{
+    public class Farmer
+    {
+        public int Id { get; set; }
+
+        [Required]
+        [Display(Name = "Full Name")]
+        public string FullName { get; set; }
+
+        [Required]
+        [Phone]
+        [Display(Name = "Phone Number")]
+        public string PhoneNumber { get; set; }
+
+        [Required]
+        [EmailAddress]
+        public string Email { get; set; }
+
+        [Display(Name = "Location")]
+        public string Location { get; set; }
+    }
+}
